93d73bec4da720010a152904f192ff6682f45f36
Thorsten Jung <tajbender@users.noreply.github.com>
2026-01-20 18:56:05 +0100

    Add `DataSource` Dummy Property. Edit NavigationView

--- Ahnenforscherin/Controls/ExplorerBrowser.cs
+++ Ahnenforscherin/Controls/ExplorerBrowser.cs
@@ -1,13 +1,7 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Documents;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -16,9 +10,24 @@ namespace Ahnenforscherin.Controls;
 
 public sealed partial class ExplorerBrowser : Control
 {
+    public object DataSource
+    {
+        get { return (object)GetValue(DataSourceProperty); }
+        set { SetValue(DataSourceProperty, value); }
+    }
+    public static readonly DependencyProperty DataSourceProperty =
+        DependencyProperty.Register("DataSource", typeof(object), typeof(ExplorerBrowser), new PropertyMetadata(null));
+
     public ExplorerBrowser()
     {
+        Debug.Print("ExplorerBrowser.ctor()");
         /* WARN: CA1416 This call site is reachable on all platforms. 'Control.DefaultStyleKey' is only supported on: 'Windows' 10.0.17763.0 and later. */
         DefaultStyleKey = typeof(ExplorerBrowser);
+
+    }
+
+    private void ExplorerBrowser_DataSourceChanged(object? sender, EventArgs e)
+    {
+        Debug.Print("ExplorerBrowser.DataSourceChanged()");
     }
 }
