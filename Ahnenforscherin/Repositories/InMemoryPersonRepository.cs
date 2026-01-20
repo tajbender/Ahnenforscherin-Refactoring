255890fdcc28b4189d359ea01bca50104cb0708f
Thorsten Jung <tajbender@users.noreply.github.com>
2026-01-20 23:00:02 +0100

    Add model dummies and repository classes

---
+++ Ahnenforscherin/Repositories/InMemoryPersonRepository.cs
@@ -0,0 +1,37 @@
+﻿using Ahnenforscherin.Models;
+using System.Collections.Generic;
+
+namespace Ahnenforscherin.Repositories;
+
+public class InMemoryPersonRepository : IPersonRepository
+{
+    private readonly Dictionary<string, Person> _persons = [];
+
+    public Person? GetById(string id)
+    {
+        _persons.TryGetValue(id, out var person);
+        return person;
+    }
+
+    public IEnumerable<Person> GetAll()
+    {
+        return _persons.Values;
+    }
+
+    public void Add(Person person)
+    {
+        if (!_persons.ContainsKey(person.Id))
+            _persons[person.Id] = person;
+    }
+
+    public void Update(Person person)
+    {
+        if (_persons.ContainsKey(person.Id))
+            _persons[person.Id] = person;
+    }
+
+    public void Delete(string id)
+    {
+        _persons.Remove(id);
+    }
+}
