a529906b7cef9ca81409056fd98cfc6cc262490e
Thorsten Jung <tajbender@users.noreply.github.com>
2026-01-26 18:46:22 +0100

    Prepare for opening SQLite database via EFCore

--- Ahnenforscherin/App.xaml.cs
+++ Ahnenforscherin/App.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿using Ahnenforscherin.Models;
+using Ahnenforscherin.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -11,11 +9,15 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Ahnenforscherin.Services;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -32,6 +34,8 @@ public partial class App : Application
 
     public Window MainWindow  { get; private set; }
 
+    public static IServiceProvider Services { get; private set; }
+
     /// <summary>
     /// Initializes the singleton application object.  This is the first line of authored code
     /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -40,6 +44,33 @@ public partial class App : Application
     {
         //InitializeComponent();
         Host = new AppHost();
+
+        var services = new ServiceCollection();
+        _ = services.AddDbContext<AppDbContext>(static options =>
+        {
+            //options.UseSqlite("Data Source=ahnenforscherin.db");
+        });
+
+        _ = services.AddScoped<IPersonRepository, PersonRepository>();
+
+        Services = services.BuildServiceProvider();
+
+
+//        var builder = Host.CreateApplicationBuilder();
+//
+//        builder.Services.AddDbContext<AppDbContext>(options =>
+//        {
+//            options.UseSqlite("Data Source=ahnenforscherin.db");
+//        });
+//
+//        builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+//
+//        var host = builder.Build();
+//        Services = host.Services;
+//
+//        InitializeComponent();
+
+
     }
 
     /// <summary>
